ce45843e5d840f17b3bcf2e9e5a783b12d1aaa87
siripatr.d <siripatr.d@riseplus.tech>
2024-02-01 10:51:35 +0700

    add data source(Db):program

--- ContosoPizza/Program.cs
+++ ContosoPizza/Program.cs
@@ -1,4 +1,5 @@
 using ContosoPizza.Services;
+using ContosoPizza.Data;
 // Additional using declarations
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,8 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<PizzaService>();
 
+builder.Services.AddSqlite<PizzaContext>("Data Source=ContosoPizza.db");
+builder.Services.AddSqlite<PromotionsContext>("Data Source=Promotions/Promotions.db");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,7 +30,8 @@ app.UseAuthorization();
 app.MapControllers();
 
 // Add the CreateDbIfNotExists method call
+app.CreateDbIfNotExists();
 
-app.MapGet("/", () => @"Contoso Pizza management API. Navigate to /swagger to open the Swagger test UI.");
+// app.MapGet("/", () => @"Contoso Pizza management API. Navigate to /swagger to open the Swagger test UI.");
 
 app.Run();
